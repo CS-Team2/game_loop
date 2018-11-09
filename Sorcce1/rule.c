213db1f6a7fd1e8ff5153393cbfca7bbc1a4157d
c0116058e9 <c0116058e9@edu.teu.ac.jp>
2018-11-09 18:07:25 +0900

    ルール説明

---
+++ Sorcce1/rule.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+	system("cls");
+	printf("１） 開始時にプレイヤーの選択を行う。\r");
+        printf("\r");
+        printf("\r");
+        printf("\r");
+        printf("\r");
+        printf("\r");
+        printf("\r");
+        printf("\r");
+        printf("\r");
+        printf("\r");
+        printf("\r");
+        printf("\r");
+        printf("\r");
+	return 0;
+}
