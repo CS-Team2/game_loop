f80ee07b8cb09bfa28dac96c545c77610b8b33dc
MOG <mogutan.ch@gmail.com>
2018-11-09 17:02:56 +0900

    キャラ,敵セットアップ(仮)

---
+++ Sorcce1/player_setup.c
@@ -0,0 +1,18 @@
+struct param {
+	char name[8];
+	int hp, mp, attack;
+};
+player_setup() {
+	if () {
+		struct param you = { "CS",100,100,100 };
+	}
+	else if () {
+		struct param you = { "BS",100,100,100 };
+	}
+	else if () {
+		struct param you = { "MS",100,100,100 };
+	}
+}
+enemy_setup() {
+	struct param enemie1 = { "enemie1",100,100,100 };
+}
