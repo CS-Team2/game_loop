fa6a2ea940956eba699019985cf54640f9767a24
MOG <mogutan.ch@gmail.com>
2018-11-09 18:13:42 +0900

    主人公、敵初期設定

--- Sorcce1/player_setup.c
+++ Sorcce1/player_setup.c
@@ -1,8 +1,7 @@
 struct param {
 	char name[8];
-<<<<<<< HEAD
 	int hp, attack, mp;
-};
+}
 player_setup() {
 		struct param CS = { "CS",75,20,10 };
 		struct param BS = { "BS",100,5,40 };
@@ -15,21 +14,5 @@ enemy_setup() {
 	struct param enemy4 = { "enemy4",110,15,4 };
 	struct param enemy5 = { "enemy5",125,15,6 };
 	struct param boss = { "boss",150,15,8 };
-=======
 	int hp, mp, attack;
-};
-player_setup() {
-	if () {
-		struct param you = { "CS",100,100,100 };
-	}
-	else if () {
-		struct param you = { "BS",100,100,100 };
-	}
-	else if () {
-		struct param you = { "MS",100,100,100 };
-	}
-}
-enemy_setup() {
-	struct param enemy1 = { "enemy1",100,100,100 };
->>>>>>> 213db1f6a7fd1e8ff5153393cbfca7bbc1a4157d
 }
